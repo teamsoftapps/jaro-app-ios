0d5f074bd71d4d9374960ec27af4d13c89ce788e
teamsoftapps <softappsio3@gmail.com>
2023-04-13 14:45:51 +0500

    update tailwind.config.js

--- App.jsx
+++ App.jsx
@@ -5,6 +5,8 @@
 //  * @format
 //  */
 
+const {default: Dashboard} = require('./src/screens/Dashboard/DashboardScreen');
+
 // import {
 //   Image,
 //   ImageBackground,
@@ -120,191 +122,14 @@
 
 // export default App;
 
-import {
-  Image,
-  ImageBackground,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import logo from './src/assets/images/Logo.png';
-import truckIcon from './src/assets/images/truckIcon.png';
-import boxIcon from './src/assets/images/truckIcon.png';
-import line from './src/assets/images/Line.png';
-import smartphone from './src/assets/images/Smartphone.png';
-import navigation from './src/assets/images/navigationIcon.png';
-import clock from './src/assets/images/clockIcon.png';
-import buldingImage from './src/assets/images/dashboard-building-bg.png';
-import logoutIcon from './src/assets/images/logoutIcon.png';
-import {Pressable} from 'react-native';
-// import {useNavigation} from '@react-navigation/native';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-import axios from 'axios';
+import {View} from 'react-native';
 
-const App = () => {
-  //   const navigation = useNavigation();
-
-  const [userInfoStorage, setUserInfoStorage] = useState();
-  const [userInfo, setUserInfo] = useState();
-
-  // console.log('navigation', userData?.route?.params?.userData);
-  // console.log('navigation1', navigation);
-
-  //   const getSingleUser = async id => {
-  //     const {data} = await axios.get(
-  //       `https://jaro-backend.herokuapp.com/api/uploadCsv/getSingleUser/${id}`,
-  //     );
-  //     if (data) {
-  //       console.log('data', data);
-  //       setUserInfo(data?.data);
-  //     }
-  //   };
-
-  //   useEffect(async () => {
-  //     setUserInfoStorage(JSON.parse(await AsyncStorage.getItem('userData')));
-  //   }, []);
-
-  //   useEffect(() => {
-  //     if (userInfoStorage) {
-  //       getSingleUser(userInfoStorage?.user?._id);
-  //     }
-  //   }, [userInfoStorage]);
-
-  // const handleLogout = async () => {
-  //   try {
-  //     await AsyncStorage.removeItem('userData');
-  //     navigation.navigate('login');
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // console.log('userInfo', userInfo);
-
-  //   console.log('user Info storage', userInfoStorage);
-  //   console.log('user info', userInfo);
+function App() {
   return (
-    <ScrollView className="h-full">
-      <View className="bg-white flex items-center pt-10 pb-5 rounded-bl-3xl rounded-br-3xl shadow-sm shadow-black">
-        <Image source={logo} style={{width: 180, height: 55}} />
-      </View>
-
-      <View className="mt-5 bg-white mx-5 rounded-3xl relative shadow-sm shadow-black">
-        <View className="pl-5 pt-5">
-          <Text className="font-semibold text-black text-xl">
-            Order Details
-          </Text>
-        </View>
-
-        <View className="flex flex-row items-center h-[170]">
-          <View className="flex flex-col items-center w-20">
-            <View>
-              <Image source={boxIcon} />
-            </View>
-
-            <View>
-              <Image source={line} />
-            </View>
-
-            <View>
-              <Image source={truckIcon} />
-            </View>
-          </View>
-
-          <View>
-            <View>
-              <Text className="text-[#808285] text-md">Order No.</Text>
-              <Text className="text-[#A71E21] text-xl font-semibold">
-                {/* #{userInfo?.orderNumber} */}
-                #5137
-              </Text>
-            </View>
-
-            <View className="mt-[40%]">
-              <Text className="text-[#808285] text-md">Pickup No.</Text>
-              <Text className="text-[#A71E21] text-xl font-semibold">
-                {/* #{userInfo?.pickupNumber} */}
-                #2137
-              </Text>
-            </View>
-          </View>
-        </View>
-
-        <View className="bg-[#A71E21] w-[60] items-center py-4 rounded-tr-3xl rounded-bl-3xl absolute top-0 right-0">
-          <Image source={smartphone} />
-        </View>
-      </View>
-
-      <View className="bg-white mt-5 mx-5 rounded-3xl relative h-1/4 flex shadow-sm shadow-black">
-        <View className="pl-5 pt-5">
-          <Text className="font-semibold text-black text-xl">
-            Location Info
-          </Text>
-        </View>
-
-        <View className="mt-5 flex flex-col justify-between h-[48%]">
-          <View
-          //   className="flex flex-row justify-self-start pl-5 mb-5"
-          >
-            <View>
-              <Image source={navigation} />
-            </View>
-
-            <Pressable
-              className="ml-3 flex items-start"
-              // onPress={() => navigation.navigate('map')}
-            >
-              <Text className="text-[#343434] font-medium text-lg p-0 mt-[-2%]">
-                {/* {userInfo?.location} */}
-                Newyork
-              </Text>
-              <Text className="p-0 m-0">{userInfo?.driverCoordinates}</Text>
-            </Pressable>
-          </View>
-
-          <View className="flex flex-row justify-self-start pl-5">
-            <View>
-              <Image source={clock} />
-            </View>
-
-            <View className="ml-2 flex items-start">
-              <Text className="text-[#343434] p-0 mt-[-3%]">
-                {/* {userInfo?.updatedAt
-                  ? userInfo?.updatedAt.split('T')[1].split('.')[0]
-                  : ' '} */}
-                11:00 pm
-              </Text>
-            </View>
-          </View>
-        </View>
-      </View>
-
-      <View className="flex relative h-[40vh] mt-5 items-center">
-        <View>
-          <Image source={buldingImage} className="h-full" />
-        </View>
-
-        <View className="absolute flex w-full items-center  flex-col group">
-          <Pressable
-            // onPress={handleLogout}
-            className="flex bg-white justify-center h-[80] w-[80] items-center rounded-full">
-            <Image source={logoutIcon} />
-          </Pressable>
-
-          <View className="flex items-center justify-center">
-            <Text className="text-[#A39696]">
-              brokered by Jaro Transportation
-            </Text>
-            <Text className="text-[#A39696] pb-10 mt-2">330-393-5659</Text>
-          </View>
-        </View>
-      </View>
-    </ScrollView>
+    <View>
+      <Dashboard />
+    </View>
   );
-};
+}
 
-export default App;
-
-const styles = StyleSheet.create({});
+export default App; 
--- src/screens/Dashboard/DashboardScreen.js
+++ src/screens/Dashboard/DashboardScreen.js
@@ -64,125 +64,122 @@ const Dashboard = userData => {
   //   console.log('user Info storage', userInfoStorage);
   //   console.log('user info', userInfo);
   return (
-    // <ScrollView className="h-full">
-    //   <View className="bg-white flex items-center pt-10 pb-5 rounded-bl-3xl rounded-br-3xl shadow-sm shadow-black">
-    //     <Image source={logo} style={{width: 180, height: 55}} />
-    //   </View>
+    <ScrollView className="h-full">
+      <View className="bg-white flex items-center pt-10 pb-5 rounded-bl-3xl rounded-br-3xl shadow-sm shadow-black">
+        <Image source={logo} style={{width: 180, height: 55}} />
+      </View>
 
-    //   <View className="mt-5 bg-white mx-5 rounded-3xl relative shadow-sm shadow-black">
-    //     <View className="pl-5 pt-5">
-    //       <Text className="font-semibold text-black text-xl">
-    //         Order Details
-    //       </Text>
-    //     </View>
+      <View className="mt-5 bg-white mx-5 rounded-3xl relative shadow-sm shadow-black">
+        <View className="pl-5 pt-5">
+          <Text className="font-semibold text-black text-xl">
+            Order Details
+          </Text>
+        </View>
 
-    //     <View className="flex flex-row items-center h-[170]">
-    //       <View className="flex flex-col items-center w-20">
-    //         <View>
-    //           <Image source={boxIcon} />
-    //         </View>
+        <View className="flex flex-row items-center h-[170]">
+          <View className="flex flex-col items-center w-20">
+            <View>
+              <Image source={boxIcon} />
+            </View>
 
-    //         <View>
-    //           <Image source={line} />
-    //         </View>
+            <View>
+              <Image source={line} />
+            </View>
 
-    //         <View>
-    //           <Image source={truckIcon} />
-    //         </View>
-    //       </View>
+            <View>
+              <Image source={truckIcon} />
+            </View>
+          </View>
 
-    //       <View>
-    //         <View>
-    //           <Text className="text-[#808285] text-md">Order No.</Text>
-    //           <Text className="text-[#A71E21] text-xl font-semibold">
-    //             {/* #{userInfo?.orderNumber} */}
-    //             #5137
-    //           </Text>
-    //         </View>
+          <View>
+            <View>
+              <Text className="text-[#808285] text-md">Order No.</Text>
+              <Text className="text-[#A71E21] text-xl font-semibold">
+                {/* #{userInfo?.orderNumber} */}
+                #5137
+              </Text>
+            </View>
 
-    //         <View className="mt-[40%]">
-    //           <Text className="text-[#808285] text-md">Pickup No.</Text>
-    //           <Text className="text-[#A71E21] text-xl font-semibold">
-    //             {/* #{userInfo?.pickupNumber} */}
-    //             #2137
-    //           </Text>
-    //         </View>
-    //       </View>
-    //     </View>
+            <View className="mt-[40%]">
+              <Text className="text-[#808285] text-md">Pickup No.</Text>
+              <Text className="text-[#A71E21] text-xl font-semibold">
+                {/* #{userInfo?.pickupNumber} */}
+                #2137
+              </Text>
+            </View>
+          </View>
+        </View>
 
-    //     <View className="bg-[#A71E21] w-[60] items-center py-4 rounded-tr-3xl rounded-bl-3xl absolute top-0 right-0">
-    //       <Image source={smartphone} />
-    //     </View>
-    //   </View>
+        <View className="bg-[#A71E21] w-[60] items-center py-4 rounded-tr-3xl rounded-bl-3xl absolute top-0 right-0">
+          <Image source={smartphone} />
+        </View>
+      </View>
 
-    //   <View className="bg-white mt-5 mx-5 rounded-3xl relative h-1/4 flex shadow-sm shadow-black">
-    //     <View className="pl-5 pt-5">
-    //       <Text className="font-semibold text-black text-xl">
-    //         Location Info
-    //       </Text>
-    //     </View>
+      <View className="bg-white mt-5 mx-5 rounded-3xl relative h-1/4 flex shadow-sm shadow-black">
+        <View className="pl-5 pt-5">
+          <Text className="font-semibold text-black text-xl">
+            Location Info
+          </Text>
+        </View>
 
-    //     <View className="mt-5 flex flex-col justify-between h-[48%]">
-    //       <View
-    //       //   className="flex flex-row justify-self-start pl-5 mb-5"
-    //       >
-    //         <View>
-    //           <Image source={navigation} />
-    //         </View>
+        <View className="mt-5 flex flex-col justify-between h-[48%]">
+          <View
+          //   className="flex flex-row justify-self-start pl-5 mb-5"
+          >
+            <View>
+              <Image source={navigation} />
+            </View>
 
-    //         <Pressable
-    //           className="ml-3 flex items-start"
-    //           // onPress={() => navigation.navigate('map')}
-    //         >
-    //           <Text className="text-[#343434] font-medium text-lg p-0 mt-[-2%]">
-    //             {/* {userInfo?.location} */}
-    //             Newyork
-    //           </Text>
-    //           <Text className="p-0 m-0">{userInfo?.driverCoordinates}</Text>
-    //         </Pressable>
-    //       </View>
+            <Pressable
+              className="ml-3 flex items-start"
+              // onPress={() => navigation.navigate('map')}
+            >
+              <Text className="text-[#343434] font-medium text-lg p-0 mt-[-2%]">
+                {/* {userInfo?.location} */}
+                Newyork
+              </Text>
+              <Text className="p-0 m-0">{userInfo?.driverCoordinates}</Text>
+            </Pressable>
+          </View>
 
-    //       <View className="flex flex-row justify-self-start pl-5">
-    //         <View>
-    //           <Image source={clock} />
-    //         </View>
+          <View className="flex flex-row justify-self-start pl-5">
+            <View>
+              <Image source={clock} />
+            </View>
 
-    //         <View className="ml-2 flex items-start">
-    //           <Text className="text-[#343434] p-0 mt-[-3%]">
-    //             {/* {userInfo?.updatedAt
-    //               ? userInfo?.updatedAt.split('T')[1].split('.')[0]
-    //               : ' '} */}
-    //             11:00 pm
-    //           </Text>
-    //         </View>
-    //       </View>
-    //     </View>
-    //   </View>
+            <View className="ml-2 flex items-start">
+              <Text className="text-[#343434] p-0 mt-[-3%]">
+                {/* {userInfo?.updatedAt
+                  ? userInfo?.updatedAt.split('T')[1].split('.')[0]
+                  : ' '} */}
+                11:00 pm
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
 
-    //   <View className="flex relative h-[40vh] mt-5 items-center">
-    //     <View>
-    //       <Image source={buldingImage} className="h-full" />
-    //     </View>
+      <View className="flex relative h-[40vh] mt-5 items-center">
+        <View>
+          <Image source={buldingImage} className="h-full" />
+        </View>
 
-    //     <View className="absolute flex w-full items-center  flex-col group">
-    //       <Pressable
-    //         // onPress={handleLogout}
-    //         className="flex bg-white justify-center h-[80] w-[80] items-center rounded-full">
-    //         <Image source={logoutIcon} />
-    //       </Pressable>
+        <View className="absolute flex w-full items-center  flex-col group">
+          <Pressable
+            // onPress={handleLogout}
+            className="flex bg-white justify-center h-[80] w-[80] items-center rounded-full">
+            <Image source={logoutIcon} />
+          </Pressable>
 
-    //       <View className="flex items-center justify-center">
-    //         <Text className="text-[#A39696]">
-    //           brokered by Jaro Transportation
-    //         </Text>
-    //         <Text className="text-[#A39696] pb-10 mt-2">330-393-5659</Text>
-    //       </View>
-    //     </View>
-    //   </View>
-    // </ScrollView>
-    <View className="flex justify-center items-center">
-      <Text className="text-center text-red-500">Muneeb</Text>
-    </View>
+          <View className="flex items-center justify-center">
+            <Text className="text-[#A39696]">
+              brokered by Jaro Transportation
+            </Text>
+            <Text className="text-[#A39696] pb-10 mt-2">330-393-5659</Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
